a75fbc47fd59cc45357e404c6b8030ffbb4edcaa
M M Aftab Chowdhury <mmaftab.chowdhury@cgi.com>
2020-01-21 09:55:46 +0100

    Data Binding: Two way binding and string interpolation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DatabindingTestComponent } from './databinding-test/databinding-test.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatabindingTestComponent
   ],
   imports: [
     BrowserModule,
